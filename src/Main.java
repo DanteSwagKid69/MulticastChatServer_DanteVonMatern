f6aa0afef50feed01f3b543666ae1916d7b48178
DanteSwagKid69 <dante.vonmatern@elev.ga.ntig.se>
2024-05-24 14:36:18 +0200

    first

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,21 @@
+import java.io.IOException;
+import java.net.MulticastSocket;
+
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+    public static void main(String[] args) throws IOException {
+
+
+        // Default port number we are going to use
+        int portNumber = 500;
+        if (args.length >= 1) {
+            portNumber = Integer.parseInt(args[0]);
+        }
+
+        // Create a MulticastSocket
+        MulticastSocket serverMulticast = new MulticastSocket(portNumber);
+        System.out.println("MulticastSocket is created at port " + portNumber);
+
+        // Determine the IP adress of host, given the host name
     }
+
 }
